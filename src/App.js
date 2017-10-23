dac4884ea33a3b2563af4d42764c2555c74904b4
Paulo Moura <paulociecomp@gmail.com>
2017-10-23 20:33:02 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+
+        <section className="hero"></section>
+
+        <main className="grid-cont">
+          <h1 className="mdc-typography--display2">Anitta</h1>
+          <h2 className="mdc-typography--title">Biografia</h2>
+
+          <div className="mdc-typography--body1">
+            <p>
+              Brazilian singer Anitta went from unknown to singing at the Olympic Games, becoming Brazil's answer to the likes of Rihanna, Beyoncé, or Katy Perry and one of the country's biggest stars. Born Larissa de Macedo Machado on March 30, 1993 and brought up in the impoverished neighborhood of Honório Gurgel in Rio de Janeiro, she began singing in church as a child and learned to dance before getting into baile funk as a teenager. She took her stage name from the central character in the TV miniseries Presença de Anita (Anita's Presence), based on the 1948 proto-Lolita novel by Mário Donato. When she was 17, one of her YouTube videos was discovered by funk Svengali and super-producer DJ Batutinha, who invited her to audition for him and subsequently signed her to his label, releasing her first single, "Eu Vou Ficar" (I'll Stay). In 2012, she was bought out of her contract by entrepreneur Kamilla Fialho, who became her manager (the two would later go on to become embroiled in a long-running multi-million-dollar legal dispute). On the strength of her single "Meiga e Abusada" (Sweet and Abused), Anitta was signed by Warner Music, who released her eponymous debut album in 2013.
+            </p>
+
+            <p>
+              The album combined the sinuous rhythms of her funk roots with a modern, American-inspired electropop-R&B sound, to great success. Her debut single for the label, "Show das Poderosas" (Powerful Girls' Show), became Brazil's most-watched YouTube video ever (130 million views) and that year's number three song on Brazilian radio. The albums Ritmo Perfeito (Perfect Rhythm) and Bang followed in swift succession, the latter going to number three in the album chart and selling over 300,000 copies, and a string of popular singles featuring a succession of well-known guest artists boosted her visibility. With her sweet, silky voice, sexy image, self-created choreography, and high-profile social media presence, she quickly became a national celebrity, but had yet bigger plans. Her first step toward an international career came in 2016, when she was selected to perform at the opening ceremony of the Olympic Games in her home city. In 2017 she recorded in English for the first time when she featured on rapper Iggy Azalea's "Switch" single; Anitta also began work on a fourth album, due for release later that year.
+            </p>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
