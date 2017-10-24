cdf337d0ff4787a85526beb4f9251b41ae8ec444
Paulo Moura <paulociecomp@gmail.com>
2017-10-24 15:24:39 -0300

    add react-tap-event-plugin

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <BrowserRouter>
